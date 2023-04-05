80cbf8304d7d270da9f68a646b6710f636ef4414
yellowsub12 <fantasm1337@gmail.com>
2023-04-04 22:43:58 -0400

    updated timer class, can be used in plane

--- Timer.cpp
+++ Timer.cpp
@@ -3,18 +3,16 @@
 Timer::Timer(int offset, int period) {
 
 	const int signal = SIGALRM;
-	sigemptyset(&sig_set); // initialize a signal set
-	sigaddset(&sig_set, signal); // add SIGALRM to the signal set
-	sigprocmask(SIG_BLOCK, &sig_set, NULL); //block the signal
+	sigemptyset(&sig_set);
+	sigaddset(&sig_set, signal);
+	sigprocmask(SIG_BLOCK, &sig_set, NULL);
 
 
-	/* set the signal event a timer expiration */
+
 	memset(&sig_event, 0, sizeof(struct sigevent));
 	sig_event.sigev_notify = SIGEV_SIGNAL;
 	sig_event.sigev_signo = signal;
-	CHID = ChannelCreate(0);
-	COID = ConnectAttach(0, 0, CHID, 0, 0);
-	SIGEV_PULSE_INIT(&event, COID, SIGEV_PULSE_PRIO_INHERIT, 1, 0);
+
 
 
 
@@ -27,13 +25,13 @@ Timer::Timer(int offset, int period) {
 }
 
 // Good
-Timer::set_timer(int offset, int period){
+void Timer::set_timer(int offset, int period){
 	timer_spec.it_value.tv_sec = offset;
 	timer_spec.it_value.tv_nsec = 0;
 	timer_spec.it_interval.tv_sec = period;
 	timer_spec.it_interval.tv_nsec = 0;
 	timer_settime(timer_id, 0, &timer_spec, NULL);
-	std::cout << "Timer has started!";
+	std::cout << "Timer has started! \n";
 }
 
 void Timer::wait_next_activation() {
@@ -42,3 +40,6 @@ void Timer::wait_next_activation() {
 	sigwait(&sig_set, &dummy);
 }
 
+Timer::~Timer() {
+	// TODO Auto-generated destructor stub
+}
